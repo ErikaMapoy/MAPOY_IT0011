b525d071c686ad8fa78101537879c68bb76503d9
ErikaMapoy <erikaaamapoy@gmail.com>
2025-01-11 11:59:15 +0800

    1st python haha

---
+++ main.py
@@ -0,0 +1,21 @@
+num1 = 10
+num2 = 20
+
+if num1 < num2:
+    print("haha galing")
+else:
+    print("mali baks")
+    
+
+listItoBaks = ["haha1", "haha2", "haha3"]
+
+print(listItoBaks)
+print(listItoBaks[1])
+print(listItoBaks[2])
+print(listItoBaks[3])
+
+
+print("-----------------------------------")
+print("          WELCOME, BADING!         ")
+print("-----------------------------------")
+
